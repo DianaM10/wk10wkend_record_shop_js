1b74b8e6e0c26447c9812cc77c26d587947891bc
Diana Man <dianaman82@gmail.com>
2016-12-03 00:06:09 +0000

    list records and find record by artist or title function

--- record_store.js
+++ record_store.js
@@ -5,4 +5,25 @@ var RecordStore = function(name, city) {
   this.cashInBank = 0;
 };
 
+RecordStore.prototype = {
+  addRecord: function(record) {
+    this.inventory.push(record);
+  },
+  listInventory: function() {
+    var inventory = this.inventory.map(function(record) {
+      return record.title +" by " + record.artist + " Price: " + record.price + "\n";
+    });
+    return inventory.toString();
+  },
+  findRecordByArtistOrTitle: function(toSell) {
+    return this.inventory.filter(function(record){
+      if((record.artist === toSell) || (record.title === toSell)) {
+        return record;
+      }
+    })
+  },
+
+  };
+ 
+
 module.exports = RecordStore;
--- specs/record_store_spec.js
+++ specs/record_store_spec.js
@@ -14,7 +14,7 @@ describe("RecordStore", function() {
     store1 = new RecordStore("Spin City", "Edinburgh");
     record1 = new Record( "Mr Scruff", "Spandex Man", 5);
     record2 = new Record( "Toots and the Maytals", "Monkey Man", 10);
-    record3 = new Record( "Toots and the Maytals", "Monkey Man", 10);
+    record3 = new Record( "Beck", "E-Pro", 10);
   });
 
   it("should have name", function() {
@@ -31,6 +31,36 @@ describe("RecordStore", function() {
 
   it("should start with zero cash in bank", function() {
     assert.equal(0, store1.cashInBank);
+  });
+
+  it("should be able to add records to inventory", function() {
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.addRecord(record3);
+    assert.equal(3, store1.inventory.length);
+  });
+
+  it("should be able to list the items in the inventory", function() {
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.addRecord(record3);
+    assert.equal("Spandex Man by Mr Scruff Price: 5" +"\n"+",Monkey Man by Toots and the Maytals Price: 10" +"\n"+ ",E-Pro by Beck Price: 10"+"\n", store1.listInventory());
+  });
+
+  it("should be able to find record by title", function() {
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.addRecord(record3);
+    var found = store1.findRecordByArtistOrTitle("Monkey Man")
+    assert.equal("Monkey Man", found[0].title );
+  });
+
+  it("should be able to find record by artist", function() {
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.addRecord(record3);
+    var found = store1.findRecordByArtistOrTitle("Toots and the Maytals")
+    assert.equal("Toots and the Maytals", found[0].artist );
   });
 
 
