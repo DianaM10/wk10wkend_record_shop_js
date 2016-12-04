78a44940ab81fd6e216dec65498fc617fe9ead3e
Diana Man <dianaman82@gmail.com>
2016-12-04 22:57:02 +0000

    collector can buy and sell records

--- recordCollector.js
+++ recordCollector.js
@@ -8,9 +8,36 @@ RecordCollector.prototype = {
   addRecord: function(record) {
     this.collection.push(record);
   },
-  buyRecord: function(record) {
-    
+
+  buyRecord: function(recordStore, record) {
+    //.find record in record store inventory and then add to this.collection. Remove payment from funds
+    var toBuy = recordStore.findRecordByArtistOrTitle(record);
+    this.funds -= toBuy[0].price;
+    this.addRecord(toBuy);
+  },
+
+  findRecordByArtistOrTitle: function(toSell) {
+    return this.collection.filter(function(record) {
+      if((record.artist === toSell) || (record.title === toSell)) {
+        return record;
+      }
+    })
+  },
+
+  sellRecord: function(toSell) {
+    //find record in collection and take price and add 30% to sell.  splice  to remove item from collection
+    var recordToSell = this.findRecordByArtistOrTitle(toSell);
+    console.log(recordToSell);
+    var markUp = recordToSell[0].price*1.3;
+    this.funds += markUp;
+    var index = this.collection.indexOf(recordToSell[0]);
+      if (index > -1) {
+        this.collection.splice(index, 1);
+      }
   }
-}
+
+  
+
+};
 
 module.exports = RecordCollector;
--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -22,8 +22,13 @@ describe("RecordCollector", function() {
     record2 = new Record( "Toots and the Maytals", "Monkey Man", 10);
     record3 = new Record( "Beck", "E-Pro", 10);
     record4 = new Record( "Fun Lovin Criminals", "Scooby Snacks" , 8 );
-    record5 = new Record( "Gomez", "Whipping Picadilly" , 12);
+    record5 = new Record( "Gomez", "Whipping Piccadilly" , 12);
     record6 = new Record( "Nirvana", "Jesus Dont Want Me For a Sunbeam", 15);
+    store1.addRecord(record1);
+    store1.addRecord(record2);
+    store1.addRecord(record3);
+
+
   });
 
   it("Should have a name", function() {
@@ -44,6 +49,27 @@ describe("RecordCollector", function() {
     collector1.addRecord(record6);
     assert.equal(3, collector1.collection.length);
   });
+
+  it("Should able buy record from store", function() {
+    collector1.addRecord(record4);
+    collector1.addRecord(record5);
+    collector1.addRecord(record6);
+    collector1.funds = 100;
+    collector1.buyRecord(store1, "Beck");
+    assert.equal(4, collector1.collection.length);
+    assert.equal(90, collector1.funds);
+  });
+
+  it("Should able sell record to store", function() {
+    collector1.addRecord(record4);
+    collector1.addRecord(record5);
+    collector1.addRecord(record6);
+    collector1.funds = 100;
+    collector1.sellRecord("Whipping Piccadilly");
+    assert.equal(2, collector1.collection.length);
+    assert.equal(115.6, collector1.funds);
+  });
+
 
 
 });
